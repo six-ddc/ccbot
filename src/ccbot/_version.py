8fd109c19091087126adcb2bf12dcb84fde3cbaa
Alexei Ledenev <alexei.led@gmail.com>
2026-02-12 08:43:00 +0200

    chore: bump version to dev62

--- src/ccbot/_version.py
+++ src/ccbot/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.dev61"
-__version_tuple__ = version_tuple = (0, 1, "dev61")
+__version__ = version = "0.1.dev62"
+__version_tuple__ = version_tuple = (0, 1, "dev62")
 
 __commit_id__ = commit_id = None
